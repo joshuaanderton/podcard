ee02e37175018dd9c12c596b214b219bd4cdbf96
Josh Upscribe <info@joshanderton.com>
2019-10-17 08:11:44 -0700

    Pricing text was incorrect

--- resources/views/editing.blade.php
+++ resources/views/editing.blade.php
@@ -38,7 +38,7 @@
                 <div class="flex flex-col md:flex-row flex-wrap mb-12">
                     <button role="link" id="checkout-button-sku_G0R7P14TtPO1JV" class="focus:outline-none flex flex-col mb-5 text-black bg-gray-200 p-6 rounded flex-1 md:mr-5">
                         <h3 class="text-2xl"><span class="font-bold">1</span> ep. One time.</h3>
-                        <div class="text-5xl font-bold mb-12">$99</div>
+                        <div class="text-5xl font-bold mb-12">$70</div>
                         <div class="mt-auto">
                             <span class="inline-flex text-white bg-black hover:text-white py-2 px-4 rounded align-items-center font-bold" href="#pricing">Get Started</span>
                         </div>
@@ -61,7 +61,7 @@
                     </button>
 
                     <button role="link" id="checkout-button-plan_G0RAsIRo61BsDn" class="focus:outline-none flex flex-col mb-5 text-black bg-gray-200 p-6 rounded flex-1">
-                        <h3 class="text-2xl"><span class="font-bold">5</span> ep. per month</h3>
+                        <h3 class="text-2xl"><span class="font-bold">8</span> ep. per month</h3>
                         <div class="text-5xl font-bold mb-12">$499/mo</div>
                         <div class="mt-auto">
                             <span class="inline-flex text-white bg-black hover:text-white py-2 px-4 rounded align-items-center font-bold" href="#pricing">Get Started</span>
