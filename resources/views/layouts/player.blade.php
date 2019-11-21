6a383edb62fa46e25f20f4b3ec37d93efe14e767
Josh Upscribe <info@joshanderton.com>
2019-11-21 12:12:17 -0800

    Remove google tracking from player

--- resources/views/layouts/player.blade.php
+++ resources/views/layouts/player.blade.php
@@ -4,25 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
         <title>{{ config('app.name', 'Laravel') }}</title>
-
         <script src="{{ asset('js/player.js') }}?v=6" defer></script>
         <link href="{{ asset('css/player.css') }}?v=6" rel="stylesheet">
         <link rel="shortcut icon" href="/favicon.png"/>
-
-        <link href="https://fonts.googleapis.com/css?family=Barlow:400,600,700,900&display=swap" rel="stylesheet">
-
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-83304079-2"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'UA-83304079-2');
-        </script>
-
     </head>
     <body>
         @yield('content')
