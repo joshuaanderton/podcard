1488427bed8522268841227b9b0a8a2b2f7463b6
Josh Upscribe <info@joshanderton.com>
2020-01-06 14:24:55 -0800

    Updated link to podcard editing

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -2,7 +2,7 @@
     <div class="p-6 flex flex-col md:flex-row text-center">
         <a class="font-sans text-xl mb-4 md:mb-0" href="{{ env('SITE_URL') }}"><strong>Podcard</strong> {{ $title }}</a>
         <div class="flex flex-col md:flex-row mx-auto md:mr-0">
-            <a class="text-black hover:text-gray-700 font-semibold md:ml-6 py-1" href="//editing.{{ env('SESSION_DOMAIN') }}">Podcast Editing</a>
+            <a class="text-black hover:text-gray-700 font-semibold md:ml-6 py-1" href="//flywheelfm.com">Podcast Editing</a>
             <a class="text-black hover:text-gray-700 font-semibold md:ml-6 py-1" href="//player.{{ env('SESSION_DOMAIN') }}">Embeddable Player</a>
             <a class="text-black hover:text-gray-700 font-semibold md:ml-6 py-1" href="//ramengames.{{ env('SESSION_DOMAIN') }}">Ramen Games</a>
         </div>
