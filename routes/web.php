819c7409188e69a863db833c1b9dc8908a8ec4ca
Joshua Anderton <info@joshanderton.com>
2022-09-07 12:53:45 -0700

    Fixed multi-domain support

--- routes/web.php
+++ routes/web.php
@@ -9,19 +9,17 @@ use App\Features\Podcasts\{
     Episodes\Embed
 };
 
-Route::domain(config('app.url'))->group(function () {
-    Route::get('/', fn () => view('player-builder'));
-    Route::get('dnt', fn () => view('pages.dnt'));
-    Route::get('ramengames', [RamenGames::class, 'handle']);
+Route::get('/',          fn () => view('player-builder'));
+Route::get('dnt',        fn () => view('pages.dnt'));
+Route::get('ramengames', RamenGames::class);
+
+Route::domain('player.' . config('app.host'))->group(function ($router) {
+    Route::get('/',          Show::class     )->name('podcasts.episodes.show');
+    Route::get('embed',      Embed::class    )->name('podcasts.episodes.embed');
+    Route::get('import',     Import::class   )->name('podcasts.import');
+    Route::get('import/all', ImportAll::class)->name('podcasts.import.all');
 });
 
-Route::domain('player.'.config('app.host'))->group(function ($router) {
-    Route::get('/', [Show::class,      'handle'])->name('podcasts.episodes.show');
-    Route::get('embed', [Embed::class,     'handle'])->name('podcasts.episodes.embed');
-    Route::get('import', [Import::class,    'handle'])->name('podcasts.import');
-    Route::get('import/all', [ImportAll::class, 'handle'])->name('podcasts.import.all');
-});
-
-Route::domain('ramengames.'.config('app.host'))->group(fn () => (
-    Route::redirect('/', config('app.url').'/ramengames')
+Route::domain('ramengames.' . config('app.host'))->group(fn () => (
+    Route::redirect('/', config('app.url') . '/ramengames')
 ));
