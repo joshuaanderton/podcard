2bcb3ff0b81df809413130f52f9184b21ce39cbd
Joshua Anderton <info@joshanderton.com>
2023-01-10 13:19:31 -0800

    Working on tests for builder

--- tests/Feature/Livewire/PlayerBuilderTest.php
+++ tests/Feature/Livewire/PlayerBuilderTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature\Livewire;
 
 use App\Http\Livewire\PlayerBuilder;
+use App\Models\Podcast;
+use App\Models\PodcastEpisode;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Livewire\Livewire;
@@ -10,10 +12,37 @@ use Tests\TestCase;
 
 class PlayerBuilderTest extends TestCase
 {
-    /** @test */
-    public function the_component_can_render()
+    use RefreshDatabase;
+
+    public function test_feed_url_pulls_in_episodes()
     {
+        $this->assertDatabaseCount(Podcast::class, 0);
+        $this->assertDatabaseCount(PodcastEpisode::class, 0);
+
         $component = Livewire::test(PlayerBuilder::class);
+
+        $component
+            ->assertSee('Episode:')
+            ->assertSeeHtml('id="iframe"')
+            ->set('feedUrl', null)
+            ->assertDontSee('Episode:')
+            ->assertSeeHtml('id="iframe"');
+
+        $component
+            ->assertSee('RSS Feed URL:')
+            ->assertDontSee('Episode:')
+            ->set('feedUrl', env('APP_URL') . '/tests/ramen.xml')
+            ->assertSee('Episode:')
+            ->assertSee('Color:');
+
+        $this->assertDatabaseCount(Podcast::class, 1);
+        $this->assertDatabaseCount(PodcastEpisode::class, 37);
+
+        $firstEpisode = PodcastEpisode::first();
+
+        $component
+            ->set('currentEpisodeId', $firstEpisode->id)
+            ->assertSee('id="iframe"');
 
         $component->assertStatus(200);
     }
