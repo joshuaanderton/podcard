7792a4f6ded1d10a9c51ede2b8420121d4a96773
Joshua Anderton <info@joshanderton.com>
2023-02-21 13:26:10 -0800

    Added freesound api helper class

---
+++ app/Services/Freesound.php
@@ -0,0 +1,19 @@
+<?php declare (strict_types=1);
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+
+class Freesound
+{
+    public static function search(?string $term = null): mixed
+    {
+        return Http::get(
+            'https://freesound.org/apiv2/search/text',
+            [
+              'token' => env('FREESOUND_API_KEY'),
+              'query' => $term
+            ]
+        )->json();
+    }
+}
