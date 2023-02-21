925eb9720f5f62c3843dccecabb50d7b1ca014ce
Joshua Anderton <info@joshanderton.com>
2023-02-21 14:10:50 -0800

    Removed demo button

--- resources/views/livewire/player-builder/toolbar.blade.php
+++ resources/views/livewire/player-builder/toolbar.blade.php
@@ -3,13 +3,7 @@
 <div class="flex flex-wrap lg:flex-nowrap items-start justify-center flex-1">
     
     <div class="min-w-full flex-1 lg:min-w-0 lg:max-w-sm">
-      <div class="flex items-center justify-between">
-        <x-jal::label text="RSS Feed URL:" class="pr-4 whitespace-nowrap" />
-        @if (! $showFields)
-          <x-jal::button text="Load Demo" wire:click="setDemoFeedUrlAndLoad" class="bg-black/10 hover:bg-black/30 -my-1" xs />
-        @endif
-      </div>
-      <x-jal::input wire:model.lazy="feedUrl" placeholder="e.g. https://feeds.transistor.fm/my-podcast" wrapper-class="mt-2">
+      <x-jal::input label="RSS Feed URL:" wire:model.lazy="feedUrl" placeholder="e.g. https://feeds.transistor.fm/my-podcast">
         @if ($feedUrl)
           <div class="my-auto">
             <button class="text-black/60 rounded-full w-6 h-6 border-none hover:bg-black/10 mr-1 inline-flex items-center justify-center" wire:click="$set('feedUrl', null)">
