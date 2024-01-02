4c639fd63b92270ed3c20371dfb231e1bbd1ff45
Joshua Anderton <info@joshanderton.com>
2024-01-02 05:55:54 -0800

    Converted back to regular repo (from mono)

--- mobile/src/pages/NoneFound.tsx
+++
@@ -1,9 +0,0 @@
-import { IonPage, IonTitle } from "@ionic/react";
-import { RouteComponentProps } from "react-router";
-
-export default ({ match }: { match: RouteComponentProps }) => (
-  <IonPage className="flex">
-    <IonTitle className="m-auto">404</IonTitle>
-    <p>{JSON.stringify(match)}</p>
-  </IonPage>
-)
